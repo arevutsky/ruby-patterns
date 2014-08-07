60b44db74d6a27b0386e918bbfaeeb462a29e73d
Alexander Revutsky <arevutsky@chegg.com>
2014-08-07 15:53:55 +0300

    Fix

--- observer/observer.rb
+++ observer/observer.rb
@@ -4,6 +4,6 @@ require './warn_low'
 
 ticker = Ticker.new
 WarnLow.new(ticker, 80)     # observer 1
-WarnLow.new(ticker, 20)     # observer 2
+WarnHigh.new(ticker, 120)   # observer 2
 WarnHigh.new(ticker, 120)   # observer 3
 ticker.run
